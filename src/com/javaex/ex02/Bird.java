ddf205a420d06a6723e0b97d4a02a92702310100
rlatjgus960 <rlatjgus960@naver.com>
2021-06-10 16:06:34 +0900

    6/10일 과제(상속, 인터페이스, 다운캐스팅)

---
+++ src/com/javaex/ex02/Bird.java
@@ -0,0 +1,30 @@
+package com.javaex.ex02;
+
+public abstract class Bird {
+
+	private String name;
+
+	public Bird() {
+		super();
+	}
+
+	public Bird(String name) {
+		super();
+		this.name = name;
+	}
+
+	protected String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public abstract void sing();
+
+	public abstract void fly();
+
+	public abstract void showName();
+
+}
---
+++ src/com/javaex/ex03/SoundApp.java
@@ -0,0 +1,17 @@
+package com.javaex.ex03;
+
+public class SoundApp {
+
+	public static void main(String[] args) {
+		printSound(new Cat());
+		printSound(new Dog());
+		printSound(new Sparrow());
+		printSound(new Duck());
+
+	}
+
+	public static void printSound(Soundable soundable) { //이거 다시
+		System.out.println(soundable.sound());
+	}
+
+}
